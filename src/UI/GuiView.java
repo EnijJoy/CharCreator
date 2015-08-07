9c244029627601fd9222adf9e3b982947c3d3627
Pavel Kondratyev <kondratyev09@yandex.ru>
2015-08-07 20:03:20 +0600

    create lite gui, horrible logic, unsafety

--- src/UI/GuiView.java
+++ src/UI/GuiView.java
@@ -1,41 +1,91 @@
 package UI;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
+import javafx.application.Application;                              //создать вкладки (file, help, options).
+import javafx.geometry.Insets;                                      //реализовать неизменняемый размер окна
+import javafx.scene.Scene;                                          //кидать exception в отдельные окна!
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import logic.ConvertNumberInChar;
 
 
 public class GuiView extends Application {
 
-    private Button button;
+    Stage window;
 
 
     public static void main(String[] args) {
         launch(args);
     }
 
-
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        primaryStage.setTitle("CharCreator v1.0");
+        window = primaryStage;
+        window.setTitle("CharCreator v1.0");
 
-        button = new Button();
-        button.setText("Generate Symbol");
-        button.setOnAction(e -> System.out.println("I learn Java & JavaFX now!"));
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(5);
+        grid.setHgap(20);
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        Label intLabel = new Label("Номер:");
+        GridPane.setConstraints(intLabel, 0, 0);
 
-        Scene scene = new Scene(layout, 300, 200);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        TextField intInput = new TextField();
+        GridPane.setConstraints(intInput, 1, 0);
 
+        Label charLabel = new Label("Символ UTF-8:");
+        GridPane.setConstraints(charLabel, 0, 1);
+
+        TextField charOut = new TextField();
+        GridPane.setConstraints(charOut, 1, 1);
+
+        Button generateButton = new Button("Generate Symbol");
+        GridPane.setConstraints(generateButton, 1, 2);
+
+        generateButton.setOnAction(e -> {
+            isInt(intInput, intInput.getText());
+            charPrint(charOut, charOut.getText());
+
+        });
+        grid.getChildren().addAll(intLabel, intInput, charLabel, charOut, generateButton);
+
+        Scene scene = new Scene(grid, 300, 200);
+        window.setScene(scene);
+
+        window.show();
     }
 
 
+    private boolean isInt(TextField input, String message) {
+        try {
+
+            int number = Integer.parseInt(input.getText());
+            ConvertNumberInChar.NUMBER_OF_CHAR = number;
+            ConvertNumberInChar.ConvertNumberInChar();
+            return true;
+
+        } catch (NumberFormatException e) {
+            System.out.println("Error: " + message + " is not a number");
+            return false;
+        }
+    }
+
+    private boolean charPrint(TextField out, String message) {
+        try {
+            StringBuilder s = new StringBuilder().append(ConvertNumberInChar.symbolUTF_8);
+            out.setText(String.valueOf(s));
+            return true;
+        } catch (NumberFormatException e) {                                  // Доработать exception (создать свой)
+            System.out.println("Error: Input a number in first TextField");  // Если ввод во второе поле, кинуть exception
+            return false;
+        }
+    }
 }
+
+
+
+
---
+++ src/logic/ConvertNumberInChar.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class ConvertNumberInChar {
+
+    public static char symbolUTF_8 = 0;     // сделать переменные приватными, реализовать безопасную логику
+
+    public static int NUMBER_OF_CHAR = 0;
+
+    public static char ConvertNumberInChar() {
+        char save = (char) NUMBER_OF_CHAR;
+        return symbolUTF_8 = save;
+    }
+
+}
